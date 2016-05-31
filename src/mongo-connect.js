21d874e72b2cd0f18a1c4939df63efe4675e0153
Felipe Lima <felipelima@nepente.io>
2016-05-31 17:12:34 -0300

    Adiciona verificação de ambiente de teste

--- src/mongo-connect.js
+++ src/mongo-connect.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 const chalk = require('chalk');
 
-const mongodbUrl = process.env.MONGODB_URI + process.env.MONGODB_NAME;
+const estaEmAmbienteDeTeste = (process.env.NODE_ENV === 'test');
+const mongodbName = estaEmAmbienteDeTeste ? `${process.env.MONGODB_NAME}_test` : process.env.MONGODB_NAME;
 const mongodbConnectionTimeout = process.env.MONGODB_CONNECTION_TIMEOUT || 30000;
 const mongodbReplicaSet = process.env.MONGODB_REPLICA_SET;
 
 if (!mongodbUrl) {
   throw new Error(chalk.red('Brô, como que eu vou me conectar sem a variável de '
     + 'ambiente MONGODB_URI. Cria o .env ou exporta no seu ambiente.'));
+}
+
+if (estaEmAmbienteDeTeste) {
+  console.log(chalk.blue('Alterado conexão de banco para _test'));
 }
 
 mongoose.Promise = global.Promise;
