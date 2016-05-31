4a90e3cdb3854ea810ce578e176819d16253b837
Felipe Lima <felipelima@nepente.io>
2016-05-31 17:46:45 -0300

    Altera retorno para promise da conexão

--- src/mongo-connect.js
+++ src/mongo-connect.js
@@ -1,54 +1,53 @@
 const mongoose = require('mongoose');
 const chalk = require('chalk');
 
-const estaEmAmbienteDeTeste = (process.env.NODE_ENV === 'test');
-const mongodbName = estaEmAmbienteDeTeste ? `${process.env.MONGODB_NAME}_test` : process.env.MONGODB_NAME;
-const mongodbUrl = `${process.env.MONGODB_URI}${mongodbName}`;
-const mongodbConnectionTimeout = process.env.MONGODB_CONNECTION_TIMEOUT || 30000;
-const mongodbReplicaSet = process.env.MONGODB_REPLICA_SET;
+const constroiUrlDeConexao = function() {
+    const estaEmAmbienteDeTeste = (process.env.NODE_ENV === 'test');
+    const mongodbName = estaEmAmbienteDeTeste ? `${process.env.MONGODB_NAME}_test` : process.env.MONGODB_NAME;
+    const mongodbUrl = `${process.env.MONGODB_URI}${mongodbName}`;
 
-if (!mongodbUrl) {
-  throw new Error(chalk.red('Brô, como que eu vou me conectar sem a variável de '
-    + 'ambiente MONGODB_URI. Cria o .env ou exporta no seu ambiente.'));
+    if (estaEmAmbienteDeTeste) {
+      console.info(chalk.blue('Alterado conexão de banco para _test'));
+    }
+
+    return mongodbUrl;
 }
 
-if (estaEmAmbienteDeTeste) {
-  console.log(chalk.blue('Alterado conexão de banco para _test'));
+const conectar = function() {
+    const mongodbUrl = constroiUrlDeConexao();
+    const mongodbConnectionTimeout = process.env.MONGODB_CONNECTION_TIMEOUT || 30000;
+    const mongodbReplicaSet = process.env.MONGODB_REPLICA_SET;
+
+    if (!mongodbUrl) {
+      throw new Error(chalk.red('Brô, como que eu vou me conectar sem a variável de '
+        + 'ambiente MONGODB_URI. Cria o .env ou exporta no seu ambiente.'));
+    }
+
+    mongoose.Promise = global.Promise;
+
+    const options = {
+      server: {
+        auto_reconnect: true,
+        socketOptions: {
+          keepAlive: 1,
+          connectTimeoutMS: mongodbConnectionTimeout,
+        },
+      },
+      replset: {
+        socketOptions: {
+          keepAlive: 1,
+          connectTimeoutMS: mongodbConnectionTimeout,
+        },
+      },
+    };
+
+    if (mongodbReplicaSet) {
+      options.replset.rs_name = mongodbReplicaSet;
+    }
+
+    console.info(chalk.green(`Conectando no servidor ${mongodbUrl}`));
+    return mongoose.connect(mongodbUrl, options);
 }
 
-mongoose.Promise = global.Promise;
-
-const options = {
-  server: {
-    auto_reconnect: true,
-    socketOptions: {
-      keepAlive: 1,
-      connectTimeoutMS: mongodbConnectionTimeout,
-    },
-  },
-  replset: {
-    socketOptions: {
-      keepAlive: 1,
-      connectTimeoutMS: mongodbConnectionTimeout,
-    },
-  },
-};
-
-if (mongodbReplicaSet) {
-  options.replset.rs_name = mongodbReplicaSet;
-}
-
-console.log(chalk.green(`Conectando no servidor ${mongodbUrl}`));
-mongoose.connect(mongodbUrl, options);
-
-const db = mongoose.connection;
-
-db.on('error', err => {
-  console.log(err);
-  console.log(err.stack);
-  throw err;
-});
-
-db.on('connected', () => {
-  console.log(chalk.green(`Conectado ao servidor ${mongodbUrl} com sucesso!`));
-});
+module.exports.conectar = conectar;
+module.exports.obterMongodbUrl = constroiUrlDeConexao;
