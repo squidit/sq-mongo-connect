0c9d7902acd0e58c28d0b8d94a4a28d151f285a6
Felipe Lima <felipelima@nepente.io>
2016-06-10 15:32:58 -0300

    Corrige geração de url quando já existe já test

--- src/constroi-url-de-conexao.js
+++ src/constroi-url-de-conexao.js
@@ -1,13 +1,18 @@
 const chalk = require('chalk');
 
+function obtemUri(mongodbName) {
+  return `${process.env.MONGODB_URI}${mongodbName}`;
+}
+
 module.exports = function constroiUrlDeConexao() {
+  const nomeOriginal = process.env.MONGODB_NAME;
   const estaEmAmbienteDeTeste = (process.env.NODE_ENV === 'test');
-  const mongodbName = estaEmAmbienteDeTeste ? `${process.env.MONGODB_NAME}_test` : process.env.MONGODB_NAME;
-  const mongodbUrl = `${process.env.MONGODB_URI}${mongodbName}`;
 
   if (estaEmAmbienteDeTeste) {
+    const mongodbName = `${nomeOriginal.replace(/_test$/, '')}_test`;
     console.info(chalk.blue(`Alterado nome de banco para ${chalk.underline(mongodbName)}`));
+    return obtemUri(mongodbName);
   }
 
-  return mongodbUrl;
+  return obtemUri(nomeOriginal);
 }
